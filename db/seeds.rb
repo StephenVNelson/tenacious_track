4790f2336a9e82cbe923598657acb68c988eee1d
StephenVNelson <StephenVNelson@gmail.com>
2018-08-25 13:53:54 -0400

    pre seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@ User.create!(name:  "Example User",
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
+  hire_date = rand(1.year).ago
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               hire_date: hire_date)
 end
