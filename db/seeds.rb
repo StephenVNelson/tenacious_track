ff3005184d9a01d6e308efd763a4721de249f40e
StephenVNelson <StephenVNelson@gmail.com>
2018-08-25 14:15:47 -0400

    post faker databse

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             hire_date: Date.today)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  hire_date = rand(1.year).ago
+  hire_date = Faker::Time.between((365*4).days.ago, Time.now, :midnight).to_date
   User.create!(name:  name,
                email: email,
                password:              password,
