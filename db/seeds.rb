0caacf2be5c25c687e328a964cef46a091647e34
StephenVNelson <StephenVNelson@gmail.com>
2018-09-12 19:24:39 -0600

    Added Sean to Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@ User.create!(name:  "Stephen Nelson",
              hire_date: Date.today, admin: true, activated: true,
              activated_at: Time.zone.now)
 
+User.create!(name:  "Sean McCawley",
+            email: "napatenacious@gmail.com",
+            password:              "password",
+            password_confirmation: "password",
+            hire_date: Date.today, admin: true, activated: true,
+            activated_at: Time.zone.now)
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
