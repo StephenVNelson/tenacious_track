73b68f966411823e5f564d64c531394ff2c1eb50
StephenVNelson <StephenVNelson@gmail.com>
2018-11-03 12:19:18 -0700

    Fixed test for element edit page

--- test/integration/element_edit_test.rb
+++ test/integration/element_edit_test.rb
@@ -4,12 +4,18 @@ class ElementEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:stephen)
     @element1 = elements(:one)
+    @category1 = element_categories(:one)
   end
+
   test "unsuccessful edit" do
     log_in_as(@user)
     get edit_element_path(@element1)
     assert_template 'elements/edit'
-    patch element_path(@element1), params: {element: {series_name: "", name: "Prone"}}
-    assert_template 'elements/edit'
+    patch element_path(@element1), params: {
+      element: {element_category: @category1,
+      name: "Prone"}}
+    assert_redirected_to elements_path
+    follow_redirect!
+    assert_template 'index'
   end
 end
